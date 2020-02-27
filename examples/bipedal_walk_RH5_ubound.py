d6daba330dca6d9b505cddaf1c57c0c611731c54
Julian Eßer <julian.esser@posteo.de>
2020-02-27 09:32:49 +0100

    Add: RH5 bounded control input and changing gait

---
+++ examples/bipedal_walk_RH5_ubound.py
@@ -0,0 +1,126 @@
+import os
+import sys
+
+import numpy as np
+
+import crocoddyl
+import example_robot_data
+import pinocchio
+from notebooks.biped_utils_rh5 import SimpleBipedGaitProblem, plotSolution
+from pinocchio.robot_wrapper import RobotWrapper
+
+WITHDISPLAY = 'display' in sys.argv or 'CROCODDYL_DISPLAY' in os.environ
+WITHPLOT = 'plot' in sys.argv or 'CROCODDYL_PLOT' in os.environ
+
+crocoddyl.switchToNumpyMatrix()
+
+# Loading the RH5 Model
+URDF_FILENAME = "RH5_Lower_Body_FixedMeshPaths.urdf"
+URDF_SUBPATH = "/abstract-smurf/urdf/" + URDF_FILENAME
+modelPath = "/home/dfki.uni-bremen.de/jesser/Dev/rh5-models"
+rh5_legs = RobotWrapper.BuildFromURDF(modelPath + URDF_SUBPATH, [modelPath], pinocchio.JointModelFreeFlyer()) # Load URDF file
+# Add the free-flyer joint limits (floating base)
+rmodel = rh5_legs.model
+ub = rmodel.upperPositionLimit
+ub[:7] = 1
+rmodel.upperPositionLimit = ub
+lb = rmodel.lowerPositionLimit
+lb[:7] = -1
+rmodel.lowerPositionLimit = lb
+
+# Artificially reduce the torque limits
+lims = rmodel.effortLimit
+# lims *= 0.5 
+# lims[11] = 70
+# lims[17] = 70
+print(lims)
+rmodel.effortLimit = lims
+
+# Setting up the 3d walking problem
+rightFoot = 'FR_SupportCenter'
+leftFoot = 'FL_SupportCenter'
+gait = SimpleBipedGaitProblem(rmodel, rightFoot, leftFoot)
+
+# Defining the initial state of the robot
+q0 = gait.q0
+v0 = pinocchio.utils.zero(rmodel.nv)
+x0 = np.concatenate([q0, v0])
+
+# Setting up all tasks
+""" GAITPHASES = \
+    [{'walking': {'stepLength': 0.6, 'stepHeight': 0.1,
+                  'timeStep': 0.03, 'stepKnots': 25, 'supportKnots': 1}},
+     {'walking': {'stepLength': 0.6, 'stepHeight': 0.1,
+                  'timeStep': 0.03, 'stepKnots': 25, 'supportKnots': 1}},
+     {'walking': {'stepLength': 0.6, 'stepHeight': 0.1,
+                  'timeStep': 0.03, 'stepKnots': 25, 'supportKnots': 1}}] """
+GAITPHASES = \
+    [{'walking': {'stepLength': 0.6, 'stepHeight': 0.1,
+                  'timeStep': 0.03, 'stepKnots': 25, 'supportKnots': 1}},
+     {'walking': {'stepLength': 1.0, 'stepHeight': 0.1,
+                  'timeStep': 0.03, 'stepKnots': 25, 'supportKnots': 1}},
+     {'walking': {'stepLength': 0.6, 'stepHeight': 0.20,
+                  'timeStep': 0.03, 'stepKnots': 25, 'supportKnots': 1}},
+     {'walking': {'stepLength': 0.6, 'stepHeight': 0.30,
+                  'timeStep': 0.03, 'stepKnots': 25, 'supportKnots': 1}}]
+cameraTF = [3., 3.68, 0.84, 0.2, 0.62, 0.72, 0.22]
+
+ddp = [None] * len(GAITPHASES)
+for i, phase in enumerate(GAITPHASES):
+    for key, value in phase.items():
+        if key == 'walking':
+            # Creating a walking problem
+            ddp[i] = crocoddyl.SolverBoxDDP(
+                gait.createWalkingProblem(x0, value['stepLength'], value['stepHeight'], value['timeStep'],
+                                          value['stepKnots'], value['supportKnots']))
+
+    # Added the callback functions
+    print('*** SOLVE ' + key + ' ***')
+    if WITHDISPLAY and WITHPLOT:
+        display = crocoddyl.GepettoDisplay(rh5_legs, 4, 4, cameraTF, frameNames=[rightFoot, leftFoot])
+        ddp[i].setCallbacks(
+            [crocoddyl.CallbackLogger(),
+             crocoddyl.CallbackVerbose(),
+             crocoddyl.CallbackDisplay(display)])
+    elif WITHDISPLAY:
+        display = crocoddyl.GepettoDisplay(rh5_legs, 4, 4, cameraTF, frameNames=[rightFoot, leftFoot])
+        ddp[i].setCallbacks([crocoddyl.CallbackVerbose(), crocoddyl.CallbackDisplay(display)])
+    elif WITHPLOT:
+        ddp[i].setCallbacks([
+            crocoddyl.CallbackLogger(),
+            crocoddyl.CallbackVerbose(),
+        ])
+    else:
+        ddp[i].setCallbacks([crocoddyl.CallbackVerbose()])
+
+    # Solving the problem with the DDP solver
+    xs = [rmodel.defaultState] * len(ddp[i].models())
+    us = [m.quasiStatic(d, rmodel.defaultState) for m, d in list(zip(ddp[i].models(), ddp[i].datas()))[:-1]]
+    ddp[i].solve(xs, us, 100, False, 0.1)
+
+    # Defining the final state as initial one for the next phase
+    x0 = ddp[i].xs[-1]
+
+# Display the entire motion
+if WITHDISPLAY:
+    display = crocoddyl.GepettoDisplay(rh5_legs, frameNames=[rightFoot, leftFoot])
+    for i, phase in enumerate(GAITPHASES):
+        display.displayFromSolver(ddp[i])
+
+# Plotting the entire motion
+if WITHPLOT:
+    plotSolution(ddp, bounds=False, figIndex=1, show=False)
+
+    for i, phase in enumerate(GAITPHASES):
+        # title = phase.keys()[0] + " (phase " + str(i) + ")"
+        title = list(phase.keys())[0] + " (phase " + str(i) + ")" #Fix python3 dict bug (TypeError: 'dict_keys' object does not support indexing) 
+        log = ddp[i].getCallbacks()[0]
+        crocoddyl.plotConvergence(log.costs,
+                                  log.u_regs,
+                                  log.x_regs,
+                                  log.grads,
+                                  log.stops,
+                                  log.steps,
+                                  figTitle=title,
+                                  figIndex=i + 3,
+                                  show=True if i == len(GAITPHASES) - 1 else False)
